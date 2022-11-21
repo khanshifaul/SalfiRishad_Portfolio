62cc4aa0b4c64bc0a7d9474f260ce758e3f875fe
Shifaul Islam <khanshifaul@gmail.com>
2022-11-21 16:53:35 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { splitVendorChunkPlugin } from 'vite'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), splitVendorChunkPlugin(), viteCompression(),],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "chunks/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      }
+    }
+  },
 })
