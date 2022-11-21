fa8474ee4322e33a999b3f7ce991ed8b54a6b11f
Shifaul Islam <khanshifaul@gmail.com>
2022-11-21 18:50:10 +0600

    modified:   src/components/HelloWorld/HelloWorld.vue

--- src/components/HelloWorld/HelloWorld.vue
+++ src/components/HelloWorld/HelloWorld.vue
@@ -8,8 +8,9 @@ import bgimg from '@/assets/bg-img/bg-img-1.jpg';
   <section class="h-screen text-white bg-local bg-cover bg-center bg-no-repeat"
     :style="{ backgroundImage: `url(${bgimg})` }">
     <HelloNavigationVue />
-    <div class="container mx-auto text-center font-extrabold pt-20">
-      <h2> I'm Salfi Bari Khan </h2>
+    <div class="container mx-auto text-left text-xl font-extrabold pt-20">
+      <span>Hi,</span>
+      <h2> I'm <span class="text-3xl"> Salfi Bari Khan </span> </h2>
       <div class="text-2xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"> Proffesional
         Photographer </div>
     </div>
