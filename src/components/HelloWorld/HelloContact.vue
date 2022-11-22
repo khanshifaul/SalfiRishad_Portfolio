d803c7bd2f102e0885e113f560364e1b3cdf38fc
Shifaul Islam <khanshifaul@gmail.com>
2022-11-22 15:20:11 +0600

    modified:   src/components/HelloWorld/HelloContact.vue

--- src/components/HelloWorld/HelloContact.vue
+++ src/components/HelloWorld/HelloContact.vue
@@ -29,7 +29,7 @@
                     class="absolute text-base text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your
                     Message</label>
             </div>
-            <input type="button" value="SEND MESSAGE" class="bg-orange p-4 rounded-full font-semibold baseline" />
+            <input type="button" value="SEND MESSAGE" class="bg-orange px-6 py-2 rounded-full font-semibold baseline" />
         </div>
     </section>
 </template>
