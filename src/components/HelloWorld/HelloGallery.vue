f9d317d85e18d1039c0386f5714d697eea4c04f8
Shifaul Islam <khanshifaul@gmail.com>
2022-11-22 16:52:45 +0600

    modified:   src/components/HelloWorld/HelloGallery.vue

--- src/components/HelloWorld/HelloGallery.vue
+++ src/components/HelloWorld/HelloGallery.vue
@@ -8,11 +8,19 @@
             <div class="bg-slate-200 px-4 py-2 rounded active:text-sky-300">Night</div>
             <div class="bg-slate-200 px-4 py-2 rounded active:text-sky-300">Portrait</div>
         </div>
-        <div class="grid grid-cols-2 py-4 space-y-2">
-            <img src="@/assets/bg-img/bg-img-2.jpg" height="auto" width="50%" alt="">
-            <img src="@/assets/bg-img/bg-img-2.jpg" height="auto" width="50%" alt="">
-            <img src="@/assets/bg-img/bg-img-2.jpg" height="auto" width="50%" alt="">
-            <img src="@/assets/bg-img/bg-img-2.jpg" height="auto" width="50%" alt="">
+        <div class="grid grid-cols-2 grid-flow-dense gap-2 py-4 place-content-center">
+            <div class="space-y-2">
+                <img src="@/assets/bg-img/bg-img-2.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-1.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-2.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-1.jpg" alt="">
+            </div>
+            <div class="space-y-2">
+                <img src="@/assets/bg-img/bg-img-1.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-2.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-1.jpg" alt="">
+                <img src="@/assets/bg-img/bg-img-2.jpg" alt="">
+            </div>
         </div>
     </section>
 </template>
